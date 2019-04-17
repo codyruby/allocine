667396245141e6eb8a5bceaa168ff61f82f8ec7a
Yousri <laifayousri@gmail.com>
2019-04-17 21:38:08 +0200

    création d'une fonction qui utilise faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Movie.destroy_all
+
+100.times do
+  movie = Movie.create!(name: Faker::Book.title, year: Faker::Number.between(1990, 2020), genre: ["action", "horreur", "comédie", "drame"].sample, synopsis: Faker::Lorem.paragraph, director: Faker::Name.name, allocine_rating: Faker::Number.decimal(1, 1), already_seen: false)
+end
+
+
